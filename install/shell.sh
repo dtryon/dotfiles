254e251b83b43abe0032ebd6abd06c5eb0c09fe2
Davin Tryon <davintryon@gmail.com>
2016-08-11 11:27:24 +0100

    add shell.sh with zsh

---
+++ install/shell.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo -e "\n\nInstalling shell..."
+echo "=============================="
+
+# zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
